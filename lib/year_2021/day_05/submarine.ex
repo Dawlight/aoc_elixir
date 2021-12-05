06d1d676b8100ee9e2fad4195916d9c2f465965b
Johan Eriksson <johan.eriksson@youcal.com>
2021-12-05 12:42:37 +0100

    Some more refactoring

--- lib/year_2021/day_05/submarine.ex
+++ lib/year_2021/day_05/submarine.ex
@@ -4,25 +4,16 @@ defmodule AdventOfCode.Year2021.Day05.Submarine do
     |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
       String.split(line, " -> ")
-      |> Enum.map(fn tuple ->
-        [x, y] =
-          String.split(tuple, ",")
-          |> Enum.map(fn number -> String.to_integer(number) end)
-
-        {x, y}
-      end)
+      |> Enum.map(fn tuple -> String.split(tuple, ",") |> Enum.map(&String.to_integer/1) end)
     end)
   end
 
-  def get_covered_coordinates([{x1, y1}, {x2, y2}] = line) do
-    x_range = x1..x2
-    y_range = y1..y2
-
+  def get_covered_coordinates([[x1, y1], [x2, y2]] = line) do
     case is_cardinal?(line) do
-      true -> for x <- x_range, y <- y_range, do: {x, y}
-      false -> Enum.zip(x_range, y_range)
+      true -> for x <- x1..x2, y <- y1..y2, do: {x, y}
+      false -> Enum.zip(x1..x2, y1..y2)
     end
   end
 
-  def is_cardinal?([{x1, y1}, {x2, y2}]), do: x1 == x2 or y1 == y2
+  def is_cardinal?([[x1, y1], [x2, y2]]), do: x1 == x2 or y1 == y2
 end
