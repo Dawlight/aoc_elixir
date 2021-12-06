6881f8a971a88aefd80de1a006f07aa3da543abf
Johan Eriksson <johan.eriksson@youcal.com>
2021-12-06 22:35:59 +0100

    Day 7 preparation

---
+++ lib/year_2021/day_07/submarine.ex
@@ -0,0 +1,5 @@
+defmodule AdventOfCode.Year2021.Day07.Submarine do
+  def parse_input(input) do
+    input
+  end
+end
---
+++ lib/year_2021/day_07/task_02/task.ex
@@ -0,0 +1,9 @@
+defmodule AdventOfCode.Year2021.Day07.Task02 do
+  alias AdventOfCode.Year2021.Day07.{
+    Submarine
+  }
+
+  def solve(input) do
+    Submarine.parse_input(input)
+  end
+end
