9543d23c8a001333ea6d48f332a0d16239d1381b
Johan Eriksson <johan.eriksson@youcal.com>
2021-12-08 11:10:30 +0100

    Day 8 refactir

--- lib/year_2021/day_08/submarine.ex
+++ lib/year_2021/day_08/submarine.ex
@@ -25,9 +25,12 @@ defmodule AdventOfCode.Year2021.Day08.Submarine do
           |> Enum.map(fn combo -> MapSet.new(String.graphemes(combo)) end)
         end)
       end)
-      |> IO.inspect(label: "")
 
-    thing =
+    calculate_sum(data)
+  end
+
+  def calculate_sum(data) do
+    baked_lines =
       for [patterns, output] <- data do
         lookup = get_lookup(patterns) |> IO.inspect(label: "LOOKUP")
 
@@ -35,18 +38,15 @@ defmodule AdventOfCode.Year2021.Day08.Submarine do
           Enum.find(lookup, fn {_number, pattern} -> MapSet.equal?(pattern, out) end)
         end
       end
+      |> IO.inspect(label: "WOW")
 
-    thing |> IO.inspect(label: "THING")
-
-    for thing2 <- thing do
-      thing2 |> IO.inspect(label: "THING")
-
-      for {number, _} <- thing2, reduce: "" do
-        grej100 ->
-          grej100 <> Integer.to_string(number)
+    for baked_line <- baked_lines do
+      for {number, _} <- baked_line, reduce: "" do
+        number_string ->
+          number_string <> Integer.to_string(number)
       end
     end
-    |> Enum.map(fn thing -> String.to_integer(thing) end)
+    |> Enum.map(fn number_string -> String.to_integer(number_string) end)
     |> Enum.sum()
   end
 
@@ -58,26 +58,37 @@ defmodule AdventOfCode.Year2021.Day08.Submarine do
     end
   end
 
-  def lookup_pattern(patterns, 0) do
-    pattern_4 = lookup_pattern(patterns, 4)
-    pattern_6 = lookup_pattern(patterns, 6)
-    nine_six_zero = Enum.filter(patterns, &pattern_with(&1, 6))
+  #   0:      1:      2:      3:      4:
+  #  aaaa    ....    aaaa    aaaa    ....
+  # b    c  .    c  .    c  .    c  b    c
+  # b    c  .    c  .    c  .    c  b    c
+  #  ....    ....    dddd    dddd    dddd
+  # e    f  .    f  e    .  .    f  .    f
+  # e    f  .    f  e    .  .    f  .    f
+  #  gggg    ....    gggg    gggg    ....
+  #
+  #   5:      6:      7:      8:      9:
+  #  aaaa    aaaa    aaaa    aaaa    aaaa
+  # b    .  b    .  .    c  b    c  b    c
+  # b    .  b    .  .    c  b    c  b    c
+  #  dddd    dddd    ....    dddd    dddd
+  # .    f  e    f  .    f  e    f  .    f
+  # .    f  e    f  .    f  e    f  .    f
+  #  gggg    gggg    ....    gggg    gggg
 
-    zero_or_nine =
-      nine_six_zero
-      |> Enum.filter(fn pattern -> !MapSet.equal?(pattern, pattern_6) end)
-      |> IO.inspect(label: "ZERO OR NINE")
+  # Only #1 contains exactly 2 segments
+  def lookup_pattern(patterns, 1), do: Enum.find(patterns, &pattern_with(&1, 2))
 
-    zero_or_nine
-    |> Enum.find(fn pattern ->
-      !MapSet.equal?(MapSet.intersection(pattern_4, pattern), pattern_4)
-    end)
-  end
+  # Only #4 contains exactly 4 segments
+  def lookup_pattern(patterns, 4), do: Enum.find(patterns, &pattern_with(&1, 4))
 
-  def lookup_pattern(patterns, 1) do
-    Enum.find(patterns, fn pattern -> MapSet.size(pattern) == 2 end)
-  end
+  # Only #7 contains exactly 3 segments
+  def lookup_pattern(patterns, 7), do: Enum.find(patterns, &pattern_with(&1, 3))
 
+  # Only #8 contains exactly 7 segments
+  def lookup_pattern(patterns, 8), do: Enum.find(patterns, &pattern_with(&1, 7))
+
+  # Only #2 is missing a certain segment (lower right)
   def lookup_pattern(patterns, 2) do
     {_missing_letter, [pattern_2]} =
       for char <- @all_characters, into: %{} do
@@ -88,29 +99,21 @@ defmodule AdventOfCode.Year2021.Day08.Submarine do
     pattern_2
   end
 
+  # Numbers with 5 segments is (#2, #3, #5)
+  # #1 is already known
+  # For (#2, #3, #5), size of intersection with #1 is 2 for #3 only
   def lookup_pattern(patterns, 3) do
     pattern_1 = lookup_pattern(patterns, 1)
-    two_three_and_five = Enum.filter(patterns, fn pattern -> MapSet.size(pattern) == 5 end)
+    two_three_and_five = Enum.filter(patterns, &pattern_with(&1, 5))
 
     Enum.find(two_three_and_five, fn two_three_or_five ->
       MapSet.size(MapSet.intersection(pattern_1, two_three_or_five)) == 2
     end)
   end
 
-  def lookup_pattern(patterns, 4) do
-    Enum.find(patterns, fn pattern -> MapSet.size(pattern) == 4 end)
-  end
-
-  def lookup_pattern(patterns, 5) do
-    two_and_three = [lookup_pattern(patterns, 2), lookup_pattern(patterns, 3)]
-
-    two_three_and_five = Enum.filter(patterns, &pattern_with(&1, 5))
-
-    Enum.find(two_three_and_five, fn two_three_or_five ->
-      !Enum.member?(two_and_three, two_three_or_five)
-    end)
-  end
-
+  # Numbers with 6 segments is (#0, #6, #9)
+  # #1 is already known
+  # For (#0, #6, #9), size of intersection with #1 is 1 for #6 only
   def lookup_pattern(patterns, 6) do
     pattern_1 = lookup_pattern(patterns, 1)
 
@@ -121,14 +124,40 @@ defmodule AdventOfCode.Year2021.Day08.Submarine do
     end)
   end
 
-  def lookup_pattern(patterns, 7) do
-    Enum.find(patterns, fn pattern -> MapSet.size(pattern) == 3 end)
+  # Numbers with 5 segments is (#2, #3, #5)
+  # #2 is already known
+  # #3 is already known
+  # The only one left is #5
+  def lookup_pattern(patterns, 5) do
+    two_and_three = MapSet.new([lookup_pattern(patterns, 2), lookup_pattern(patterns, 3)])
+    two_three_and_five = MapSet.new(Enum.filter(patterns, &pattern_with(&1, 5)))
+
+    MapSet.difference(two_three_and_five, two_and_three) |> MapSet.to_list() |> List.first()
   end
 
-  def lookup_pattern(patterns, 8) do
-    Enum.find(patterns, fn pattern -> MapSet.size(pattern) == 7 end)
+  # Number with 6 segments is (#0, #6, #9)
+  # 6 is already known (can be excluded)
+  # 4 is already known
+  # Intersection of #9 and #4 -> #4, but not for #0
+  def lookup_pattern(patterns, 0) do
+    pattern_4 = lookup_pattern(patterns, 4)
+    pattern_6 = lookup_pattern(patterns, 6)
+    nine_six_zero = Enum.filter(patterns, &pattern_with(&1, 6))
+
+    zero_and_nine =
+      nine_six_zero
+      |> Enum.filter(fn pattern -> !MapSet.equal?(pattern, pattern_6) end)
+
+    zero_and_nine
+    |> Enum.find(fn pattern ->
+      !MapSet.equal?(MapSet.intersection(pattern_4, pattern), pattern_4)
+    end)
   end
 
+  # Number with 6 segments is (#0, #6, #9)
+  # #6 is already known (can be excluded)
+  # #4 is already known
+  # Intersection of #9 and #4 -> 4, but not for #0
   def lookup_pattern(patterns, 9) do
     pattern_4 = lookup_pattern(patterns, 4)
     pattern_6 = lookup_pattern(patterns, 6)
@@ -137,16 +166,11 @@ defmodule AdventOfCode.Year2021.Day08.Submarine do
     zero_or_nine =
       nine_six_zero
       |> Enum.filter(fn pattern -> !MapSet.equal?(pattern, pattern_6) end)
-      |> IO.inspect(label: "ZERO OR NINE")
 
     zero_or_nine
     |> Enum.find(fn pattern ->
       MapSet.equal?(MapSet.intersection(pattern_4, pattern), pattern_4)
     end)
-  end
-
-  def lookup_pattern(_patterns, _) do
-    nil
   end
 
   def pattern_with(pattern, number_of_segments) do
