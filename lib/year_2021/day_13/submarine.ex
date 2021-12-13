982361d8cea5a812434715a8a8f941e12cad60f5
Johan Eriksson <johan.eriksson@youcal.com>
2021-12-13 11:26:20 +0100

    Day 13

--- lib/year_2021/day_13/submarine.ex
+++ lib/year_2021/day_13/submarine.ex
@@ -1,5 +1,25 @@
 defmodule AdventOfCode.Year2021.Day13.Submarine do
   def parse_input(input) do
-    input
+    [dots, folds] = input |> String.split("\n\n")
+
+    matrix =
+      dots
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn coord_string ->
+        coord_string
+        |> String.split(",", trim: true)
+        |> Enum.map(fn number -> String.to_integer(number) end)
+      end)
+      |> MapSet.new()
+
+    folds =
+      folds
+      |> String.split("\n")
+      |> Enum.map(fn "fold along " <> instruction ->
+        [axis, coordinate] = instruction |> String.split("=")
+        {axis, coordinate |> String.to_integer()}
+      end)
+
+    {matrix, folds} |> IO.inspect(label: "WOW")
   end
 end
